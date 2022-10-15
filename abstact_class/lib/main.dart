23f96e6ff8afde757fea8a676ee10a69b12a7114
ezhuezhu07 <ezhun@egnaroinc.com>
2022-10-15 12:51:30 +0530

    Step4 in abstract class

--- abstact_class/lib/main.dart
+++ abstact_class/lib/main.dart
@@ -5,7 +5,11 @@ extension Log on Object {
   void log() => devvtools.log(toString());
 }
 
+enum Type { cat, dog }
+
 abstract class CanRun {
+  final Type clsType;
+  const CanRun({required this.clsType});
   String get type {
     if (this is Cat) {
       return 'cat';
@@ -23,6 +27,7 @@ abstract class CanRun {
 // Any class that extends this absract class should implement the abstract method (run method should be implemented)
 
 class Cat extends CanRun {
+  const Cat() : super(clsType: Type.cat);
   @override
   void run() {
     // TODO super.run(); if we want to call the the abstract method of CanRun class
@@ -31,17 +36,21 @@ class Cat extends CanRun {
   }
 }
 
-class Dog extends CanRun {}
+class Dog extends CanRun {
+  const Dog() : super(clsType: Type.dog);
+}
 
 // ! extends - inherit from particular class (max 1)
 // ! with - The with keyword allow you to mix a "mixin" with your class. [inherit more than 1 class]
-
+// ! Abstract class can have a constructor
+// ! Abstract class without constructor can act as mixins
+// ! Abstract class with constructor cannot be a mixin
 void testIt() {
   'testit called'.log();
   final cat = Cat();
-  cat.type.log();
+  cat.clsType.log();
   final dog = Dog();
-  dog.type.log();
+  dog.clsType.log();
 }
 
 void main() {
